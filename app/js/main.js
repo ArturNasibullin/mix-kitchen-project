f8ced10ed9301aaf37465145cf75ab71e83b9c6e
Artur <nasibullin.artur@gmail.com>
2021-06-18 21:40:00 +0500

    add modal and catalog.html

--- app/js/main.js
+++ app/js/main.js
@@ -1,119 +1,179 @@
 window.addEventListener('DOMContentLoaded', () => {
-	// Табы в блоке Services
-	function tabs(tabsSelector) {
-		const tabs = document.querySelectorAll(tabsSelector),
-			tabsContent = document.querySelectorAll('.description__item'),
-			tabsParent = document.querySelector('#services__tabs');
+	let menuBtn = function () {
+		let burger = document.querySelector('.header__nav-btn');
+		let menu = document.querySelector('.header-mobile');
+		let menuLinks = document.querySelectorAll('.header-mobile-menu__link');
 
-		function hideTabsContent() {
-			tabsContent.forEach((content) => {
-				content.style.display = 'none';
-			});
+		burger.addEventListener('click', () => {
+			menu.classList.toggle('active');
+			burger.classList.toggle('active');
 
-			tabs.forEach((item) => {
-				item.classList.remove('services__tabs-item--active');
-			});
-		}
-
-		function showTabs(i = 0) {
-			tabsContent[i].style.display = 'block';
-			tabs[i].classList.add('services__tabs-item--active');
-		}
-
-		hideTabsContent();
-		showTabs();
-
-		tabsParent.addEventListener('click', (event) => {
-			const target = event.target;
-
-			if (target && target.classList.contains(tabsSelector.slice(1))) {
-				tabs.forEach((item, i) => {
-					if (target == item) {
-						hideTabsContent();
-						showTabs(i);
-					}
+			menuLinks.forEach((link) => {
+				link.addEventListener('click', () => {
+					menu.classList.remove('active');
+					burger.classList.remove('active');
+					document.body.style.overflow = '';
 				});
+			});
+			// Блокировать прокрутку экрана при активном Меню
+			if (menu.classList.contains('active')) {
+				document.body.style.overflow = 'hidden';
+			} else {
+				document.body.style.overflow = '';
 			}
 		});
-	}
+	};
 
-	tabs('.services__tabs-item');
+	let modalCalc = () => {
+		let furnitureHeader = document.querySelectorAll('.furniture-cards__header');
+		let furnitureBtn = document.querySelectorAll('.furniture-cards__btn');
+		let modalImg = document.querySelector('.modal-cal__img');
+		let modalText = document.querySelector('.modal-calc__text');
 
-	//Smooth Scroll
-
-	function scrollTo() {
-		const links = document.querySelectorAll('.header-menu__link');
-		links.forEach((each) => (each.onclick = scrollAnchors));
-	}
-
-	function scrollAnchors(e, respond = null) {
-		const distanceToTop = (el) => Math.floor(el.getBoundingClientRect().top);
-		e.preventDefault();
-		var targetID = respond ? respond.getAttribute('href') : this.getAttribute('href');
-		const targetAnchor = document.querySelector(targetID);
-		if (!targetAnchor) return;
-		const originalTop = distanceToTop(targetAnchor);
-		window.scrollBy({ top: originalTop - 30, left: 0, behavior: 'smooth' });
-		const checkIfDone = setInterval(function () {
-			const atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
-			if (distanceToTop(targetAnchor) === 0 || atBottom) {
-				targetAnchor.tabIndex = '-1';
-				targetAnchor.focus();
-				window.history.pushState('', '', targetID);
-				clearInterval(checkIfDone);
+		function modalInner(target) {
+			for (let i = 0; i < furnitureHeader.length; i++) {
+				if (furnitureHeader[i].dataset.id == target) {
+					switch (furnitureHeader[i].dataset.id) {
+						case 'ldsp':
+							modalText.innerHTML = 'Кухни из ЛДСП';
+							modalImg.src = 'img/furniture/ldsp.jpg';
+							break;
+						case 'mdf':
+							modalText.innerHTML = 'Кухни из МДФ';
+							modalImg.src = 'img/furniture/mdf.jpg';
+							break;
+						case 'emal':
+							modalText.innerHTML = 'Кухни покрытые эмалью';
+							modalImg.src = 'img/furniture/emal.jpg';
+							break;
+						case 'blum':
+							modalText.innerHTML = 'Кухни с фурнитурой Blum';
+							modalImg.src = 'img/furniture/blum.jpg';
+							break;
+						case 'het':
+							modalText.innerHTML = 'Кухни с фурнитурой Hettich';
+							modalImg.src = 'img/furniture/hettich.png';
+							break;
+						case 'boyard':
+							modalText.innerHTML = 'Кухни с фурнитурой BOYARD';
+							modalImg.src = 'img/furniture/boyard.png';
+							break;
+					}
+				}
 			}
-		}, 100);
-	}
-	scrollTo();
+		}
+		modalInner();
 
-	// Кнопка menu
-	let btn = document.querySelector('.header__nav-btn');
-	let menu = document.querySelector('.header-mobile');
-	let menuItem = document.querySelectorAll('.header__menu-link');
-
-	btn.addEventListener('click', () => {
-		menu.classList.toggle('active');
-		btn.classList.toggle('active');
-
-		menuItem.forEach((item) => {
-			item.addEventListener('click', () => {
-				btn.classList.remove('active');
-				menu.classList.remove('active');
+		furnitureBtn.forEach((btn) => {
+			btn.addEventListener('click', (event) => {
+				const target = event.target.dataset.id;
+				modalInner(target);
 			});
 		});
-		// Блокировать прокрутку экрана при активном Меню
-		if (menu.classList.contains('active')) {
+	};
+
+	let modal = () => {
+		let connectBtn = document.querySelectorAll('.modal-open');
+		connectBtn.forEach((btn) => {
+			btn.addEventListener('click', () => {
+				if (btn.dataset.connect == 'calc') {
+					openModal('.modal--calc');
+				} else if (btn.dataset.connect == 'callback') {
+					openModal('.modal--callback');
+				}
+			});
+		});
+		function openModal(window) {
+			let modalWindow = document.querySelector(window);
+			modalWindow.style.opacity = 1;
+			modalWindow.style.visibility = 'visible';
 			document.body.style.overflow = 'hidden';
-		} else {
+		}
+		function closeModal(window) {
+			window.style.opacity = 0;
+			window.style.visibility = 'hidden';
 			document.body.style.overflow = '';
 		}
-	});
-
-	let animatedItem = document.querySelectorAll('.animate__animated');
-	animatedItem.forEach((item) => {
-		item.style.opacity = 0;
-		let effect = item.dataset.effect;
-		var waypoint = new Waypoint({
-			element: item,
-			handler: function (direction) {
-				if (effect === 'fadeInUp') {
-					item.classList.add('animate__fadeInUp');
-				} else if (effect === 'fadeInLeft') {
-					item.classList.add('animate__fadeInLeft');
-				} else if (effect === 'fadeInRight') {
-					item.classList.add('animate__fadeInRight');
-				} else if (effect === 'flipInX') {
-					item.classList.add('animate__flipInX');
-					item.style.opacity = 1;
-				} else if (effect === 'zoomIn') {
-					item.classList.add('animate__zoomIn');
-					item.style.opacity = 1;
-				} else if (effect === 'pulse') {
-					item.classList.add('animate__pulse');
-					item.style.opacity = 1;
-				}
-			},
-			offset: '75%',
+		document.addEventListener('click', (event) => {
+			let target = event.target;
+			if (target.classList.contains('modal__close') || target.classList.contains('modal__overlay')) {
+				let parent = target.closest('.modal');
+				closeModal(parent);
+				document.body.style.overflow = '';
+			}
 		});
-	});
+	};
+	modal();
+	menuBtn();
+	modalCalc();
+
+	// Input Masks
+	// VMasker(document.querySelectorAll('input[type="tel"]')).maskPattern('+9 (999) 999-99-99');
+
+	// let animatedItem = document.querySelectorAll('.animate__animated');
+	// animatedItem.forEach((item) => {
+	// 	item.style.opacity = 0;
+	// 	let effect = item.dataset.effect;
+	// 	var waypoint = new Waypoint({
+	// 		element: item,
+	// 		handler: function (direction) {
+	// 			if (effect === 'fadeInUp') {
+	// 				item.classList.add('animate__fadeInUp');
+	// 			} else if (effect === 'fadeInLeft') {
+	// 				item.classList.add('animate__fadeInLeft');
+	// 			} else if (effect === 'fadeInRight') {
+	// 				item.classList.add('animate__fadeInRight');
+	// 			} else if (effect === 'flipInX') {
+	// 				item.classList.add('animate__flipInX');
+	// 				item.style.opacity = 1;
+	// 			} else if (effect === 'zoomIn') {
+	// 				item.classList.add('animate__zoomIn');
+	// 				item.style.opacity = 1;
+	// 			} else if (effect === 'pulse') {
+	// 				item.classList.add('animate__pulse');
+	// 				item.style.opacity = 1;
+	// 			}
+	// 		},
+	// 		offset: '75%',
+	// 	});
+	// });
+
+	// Табы в блоке Services
+	// function tabs(tabsSelector) {
+	// 	const tabs = document.querySelectorAll(tabsSelector),
+	// 		tabsContent = document.querySelectorAll('.description__item'),
+	// 		tabsParent = document.querySelector('#services__tabs');
+
+	// 	function hideTabsContent() {
+	// 		tabsContent.forEach((content) => {
+	// 			content.style.display = 'none';
+	// 		});
+
+	// 		tabs.forEach((item) => {
+	// 			item.classList.remove('services__tabs-item--active');
+	// 		});
+	// 	}
+
+	// 	function showTabs(i = 0) {
+	// 		tabsContent[i].style.display = 'block';
+	// 		tabs[i].classList.add('services__tabs-item--active');
+	// 	}
+
+	// 	hideTabsContent();
+	// 	showTabs();
+
+	// 	tabsParent.addEventListener('click', (event) => {
+	// 		const target = event.target;
+
+	// 		if (target && target.classList.contains(tabsSelector.slice(1))) {
+	// 			tabs.forEach((item, i) => {
+	// 				if (target == item) {
+	// 					hideTabsContent();
+	// 					showTabs(i);
+	// 				}
+	// 			});
+	// 		}
+	// 	});
+	// }
+	// tabs('.services__tabs-item');
 });
