b1ae4207d6611ffc837b2ecdd9ab90d96cffe1bc
Artur <nasibullin.artur@gmail.com>
2021-10-15 12:45:36 +0500

    add contacts

--- app/js/main.js
+++ app/js/main.js
@@ -142,20 +142,20 @@ window.addEventListener('DOMContentLoaded', () => {
 	}
 
 	// accordion
-	var acc = document.querySelectorAll(".decor-descr-accordion__item");
+	var acc = document.querySelectorAll('.decor-descr-accordion__item');
 	var i;
 
 	for (i = 0; i < acc.length; i++) {
-		acc[i].addEventListener("click", function() {
-		  this.classList.toggle("active");
-		  var panel = this.nextElementSibling;
-		  if (panel.style.maxHeight){
-			 panel.style.maxHeight = null;
-		  } else {
-			 panel.style.maxHeight = panel.scrollHeight + "px";
-		  } 
+		acc[i].addEventListener('click', function () {
+			this.classList.toggle('active');
+			var panel = this.nextElementSibling;
+			if (panel.style.maxHeight) {
+				panel.style.maxHeight = null;
+			} else {
+				panel.style.maxHeight = panel.scrollHeight + 'px';
+			}
 		});
-	 }
+	}
 	tabs('.decor-tabs__item');
 	modal();
 	menuBtn();
